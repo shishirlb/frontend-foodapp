89eaf329ff0e7a575e46094e95600d8adc1bbbbe
shishirlb <114382895+shishirlb@users.noreply.github.com>
2022-10-04 17:51:58 +0530

    added product state

--- frontend/src/app/shared/menu-items.ts
+++ frontend/src/app/shared/menu-items.ts
@@ -1,19 +1,20 @@
 import { Injectable } from "@angular/core";
 
-export interface Menu{
-    state:string;
-    name:string;
-    icon:string;
-    role:string;
+export interface Menu {
+    state: string;
+    name: string;
+    icon: string;
+    role: string;
 }
 
-const MENUITEMS=[
-    {state: 'dashboard',name: 'Dashboard',icon: 'dashboard',role:''}
+const MENUITEMS = [
+    { state: 'dashboard', name: 'Dashboard', icon: 'dashboard', role: '' },
+    { state: 'product', name: 'Manage Product', icon: 'inventory_2', role: 'branchmanager' }
 ];
 
 @Injectable()
-export class MenuItems{
-    getMenuItem():Menu[]{
+export class MenuItems {
+    getMenuItem(): Menu[] {
         return MENUITEMS;
     }
 }
