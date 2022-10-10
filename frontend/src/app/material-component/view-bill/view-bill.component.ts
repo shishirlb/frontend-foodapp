ad740337e1f5410c75e79b1ab7ed0c730635ee6a
shishirlb <114382895+shishirlb@users.noreply.github.com>
2022-10-10 22:16:35 +0530

    Minor changes

--- frontend/src/app/material-component/view-bill/view-bill.component.ts
+++ frontend/src/app/material-component/view-bill/view-bill.component.ts
@@ -16,103 +16,99 @@ import { ViewBillProductsComponent } from '../dialog/view-bill-products/view-bil
   styleUrls: ['./view-bill.component.scss']
 })
 export class ViewBillComponent implements OnInit {
-  displayedColumns: string[] = ['name','email','contactNumber','paymentMethod','total','view'];
-  dataSource:any;
-  responseMessage:any;
+  displayedColumns: string[] = ['name', 'email', 'contactNumber', 'paymentMethod', 'total', 'view'];
+  dataSource: any;
+  responseMessage: any;
 
-  constructor(private billService:BillService,
-    
-    private dialog:MatDialog,
-    private snackbarService:SnackbarService,
-    private router:Router) { }
+  constructor(private billService: BillService,
+    private dialog: MatDialog,
+    private snackbarService: SnackbarService,
+    private router: Router) { }
 
   ngOnInit(): void {
-     this.tableData();
+    this.tableData();
   }
-  tableData(){
-    this.billService.getBills().subscribe((response:any)=>{
+
+  tableData() {
+    this.billService.getBills().subscribe((response: any) => {
       this.dataSource = new MatTableDataSource(response);
-      
-    
-    },(error:any)=>{
-      if(error.error?.message){
+    }, (error: any) => {
+      if (error.error?.message) {
         this.responseMessage = error.error?.message;
       }
-      else{
+      else {
         this.responseMessage = GlobalConstants.genericError;
       }
-      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
+      this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
     })
-     
+
   }
-  applyFilter(event:Event){
+  
+  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter= filterValue.trim().toLowerCase();
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-  handleViewAction(Values:any){
+
+  handleViewAction(values: any) {
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.data ={
-      data:Values
-  };
-  dialogConfig.width ="100%";
-  const dialogRef = this.dialog.open(ViewBillProductsComponent,dialogConfig);
-  this.router.events.subscribe(()=>{
-    dialogRef.close();
-  })
+    dialogConfig.data = {
+      data: values
+    };
+    dialogConfig.width = "100%";
+    const dialogRef = this.dialog.open(ViewBillProductsComponent, dialogConfig);
+    this.router.events.subscribe(() => {
+      dialogRef.close();
+    })
   }
-  downloadReportAction(values:any){
+
+  downloadReportAction(values: any) {
     var data = {
-
-      name:values.name,
-      
-      email:values.email, uuid:values.uuid,      
-      contactNumber:values.contactNumber,
-      paymentMethod: values.paymentMethod, totalAmount:values.total,
-      productDetails:values.productDetails
+      name: values.name,
+      email: values.email, 
+      uuid: values.uuid,
+      contactNumber: values.contactNumber,
+      paymentMethod: values.paymentMethod, totalAmount: values.total,
+      productDetails: values.productDetails
     }
-     this.billService.getPDF(data).subscribe(
-      (response)=>{
-        saveAs(response,values.uuid+'.pdf');
-        
+    this.billService.getPDF(data).subscribe(
+      (response) => {
+        saveAs(response, values.uuid + '.pdf');
       }
-     )
+    )
   }
 
-  handleDeleteAction(values:any){
-    
+  handleDeleteAction(values: any) {
+
     const dialogConfig = new MatDialogConfig();
 
-dialogConfig.data = {
-message: 'delete' +values.name+' bill'
-};
-const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig); 
-const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((response)=>{
-  
-  this.deleteProduct(values.id);
-  dialogRef.close();
-})
-}
-deleteProduct(id:any){
-  this.billService.delete(id).subscribe((response:any)=>{
+    dialogConfig.data = {
+      message: 'delete' + values.name + ' bill'
+    };
+    const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((response) => {
 
-  
-    
-    this.tableData();
-    
-    this.responseMessage = response?.message;
-    
-    this.snackbarService.openSnackBar (this.responseMessage, "Success");
-    
-},(error:any)=>{
-  if(error.error?.message){
-    this.responseMessage = error.error?.message;
+      this.deleteProduct(values.id);
+      dialogRef.close();
+    })
   }
-  else{
-    this.responseMessage = GlobalConstants.genericError;
-  }
-  this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
-  })
+  deleteProduct(id: any) {
+    this.billService.delete(id).subscribe((response: any) => {
+      this.tableData();
 
- 
-}
+      this.responseMessage = response?.message;
+
+      this.snackbarService.openSnackBar(this.responseMessage, "Success");
+
+    }, (error: any) => {
+      if (error.error?.message) {
+        this.responseMessage = error.error?.message;
+      }
+      else {
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
+
+
+  }
 }
